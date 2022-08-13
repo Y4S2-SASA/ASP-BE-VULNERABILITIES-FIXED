5ae81d4957a5794cd0419d887eac734b648a5c9c
amanthajayathilake <amanthajayathilake@gmail.com>
2022-08-13 23:00:36 +0530

    User login and registration implementation

--- index.js
+++ index.js
@@ -2,20 +2,24 @@ require("dotenv").config();
 const express = require("express");
 const App = express();
 const cors = require("cors");
-// const Connection = require("./db");
+const Connection = require("./db");
+const userRouter = require("./routes/users");
+const authRouter = require("./routes/auth");
 const chalk = require("chalk");
 
 //just a console style. :(
 const portCon = chalk.bold.red;
 
 // database connection
-// Connection();
+Connection();
 
 // middlewares
 App.use(express.json());
 App.use(cors());
 
 // routes
+App.use("/register", userRouter);
+App.use("/login", authRouter);
 
 const port = process.env.PORT || 3001;
 App.listen(port, console.log(portCon(`🚀 Server listening on PORT ${process.env.PORT} 🚀`)));
