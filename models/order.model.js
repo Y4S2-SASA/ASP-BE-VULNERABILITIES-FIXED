9b14572aa591507b86d04ce9329ce2162d5b0def
Dulshan Perera <dulshanperera55@gmail.com>
2022-08-14 16:09:04 +0530

    Order model created

---
+++ models/order.model.js
@@ -0,0 +1,62 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const OrderItemSchema = new Schema({
+    itemId:{
+        type: String,
+        required: true
+    },
+    userId:{
+        type: String,
+        required: true
+    },
+    itemName:{
+        type: String
+    },
+    quantity: {
+        type: Number
+    },
+    price: {
+        type: String
+    },
+    imageUrl: {
+        type: String
+    }
+})
+
+const OrderSchema = new Schema({
+        orderId: {
+            type: String,
+            required: true
+        },
+        userId:{
+            type: String,
+            required: true
+        },
+        firstName: {
+            type: String
+        },
+        lastName: {
+            type: String
+        },
+        email: {
+            type: String
+        },
+        contactNo: {
+            type: String
+        },
+
+        items : [OrderItemSchema],
+
+        status: {
+            type: String
+        },
+        total:{
+            type: String
+        }
+    },
+    { timestamps: true }
+);
+
+export const Order = mongoose.mode("Orders", OrderSchema);
